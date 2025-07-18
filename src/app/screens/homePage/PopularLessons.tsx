584cd09a408597360613680433e716137dcb6ae5
sherzod992 <sherzodbek.sharobiddinov1@gmail.com>
2025-07-19 01:19:23 +0900

    homepage

--- src/app/screens/homePage/PopularLessons.tsx
+++ src/app/screens/homePage/PopularLessons.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import { Box, Container, Stack } from "@mui/material";
-import Card from "@mui/joy/Card";
-import CardCover from "@mui/joy/CardCover";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
-import { CssVarsProvider } from "@mui/joy/styles";
+import {
+  Box,
+  Container,
+  Stack,
+  Divider
+} from "@mui/material";
+import {
+  Card,
+  CardCover,
+  CardContent,
+  Typography,
+  CardOverflow,
+  AspectRatio,
+  CssVarsProvider
+} from "@mui/joy";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import { CardOverflow } from "@mui/joy";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
-
 import { useSelector } from "react-redux";
 import { createSelector } from "@reduxjs/toolkit";
 import { retriverPopularLessons } from "./selector.ts";
@@ -24,12 +31,10 @@ const popularLessonsRetriever = createSelector(
 
 export default function PopularLessons() {
   const { popularLessons } = useSelector(popularLessonsRetriever);
-
   const [selectedVideos, setSelectedVideos] = useState<string[] | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
 
   const handleOpenModal = (lessonVideo: string[]) => {
-    console.log("Modal ochilmoqda", lessonVideo);
     setSelectedVideos(lessonVideo);
     setModalOpen(true);
   };
@@ -45,7 +50,7 @@ export default function PopularLessons() {
         <Stack className="popular-section">
           <Box className="category-title">Popular Lessons</Box>
 
-          <Stack className="cards-frame" spacing={2}>
+          <Stack className="cards-frame" direction="row" flexWrap="wrap" gap={3}>
             {popularLessons.length !== 0 ? (
               popularLessons.map((lesson: Lesson) => {
                 const imagePath = lesson.lessonImages?.[0]
@@ -55,60 +60,68 @@ export default function PopularLessons() {
                 return (
                   <CssVarsProvider key={lesson._id}>
                     <Card
-                      className="card"
-                      onClick={() => handleOpenModal(lesson.lessonVideo)}
-                      sx={{ cursor: "pointer" }}
                       variant="outlined"
+                      sx={{ width: 320, cursor: "pointer" }}
+                      onClick={() => handleOpenModal(lesson.lessonVideo)}
                     >
-                      <CardCover>
-                        <img src={imagePath} alt={lesson.lessonDesc} />
-                      </CardCover>
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img
+                            src={imagePath}
+                            loading="lazy"
+                            alt={lesson.lessonTitle}
+                          />
+                        </AspectRatio>
+                      </CardOverflow>
 
-                      <CardCover className="card-cover" />
-
-                      <CardContent sx={{ justifyContent: "flex-end" }}>
-                        <Stack flexDirection={"row"} justifyContent={"space-between"}>
-                          <Typography level="h2" textColor="#fff" fontSize={"lg"} mb={1}>
-                            {lesson.lessonTitle}
-                          </Typography>
-
-                          <Typography
-                            sx={{
-                              fontWeight: "md",
-                              color: "neutral.300",
-                              alignItems: "center",
-                              display: "flex",
-                            }}
-                          >
-                            {lesson.lessonViews}
-                            <VisibilityIcon sx={{ fontSize: 25, marginLeft: "5px" }} />
-                          </Typography>
-                        </Stack>
+                      <CardContent>
+                        <Typography
+                          sx={{
+                            fontWeight: 'bold',
+                            color: 'black',
+                            fontSize: '1.3rem',
+                            mb: 0.5
+                          }}
+                        >
+                          ${lesson.lessonPrice}
+                        </Typography>
+                        <Typography level="title-md">{lesson.lessonTitle}</Typography>
+                        <Typography level="body-xs" sx={{ mt: 0.5 }}>
+                          {lesson.lessonName} Lessons
+                        </Typography>
                       </CardContent>
 
-                      <CardOverflow
-                        sx={{
-                          display: "flex",
-                          gap: 1.5,
-                          py: 1.5,
-                          px: "var(--Card-padding)",
-                          borderTop: "1px solid",
-                          height: "60px",
-                        }}
-                      >
-                        <Typography
-                          startDecorator={<DescriptionOutlinedIcon />}
-                          textColor={"neutral.300"}
-                        >
-                          {lesson.lessonDesc}
-                        </Typography>
+                      <CardOverflow variant="soft" sx={{ bgcolor: 'background.level2' }}>
+                        <Divider inset="context" />
+                        <CardContent orientation="horizontal" sx={{ gap: 1.5 }}>
+
+                          <Divider orientation="vertical" />
+                          <Typography
+                            level="body-xs"
+                            textColor="text.secondary"
+                            sx={{ fontWeight: 'md', display: 'flex', alignItems: 'center' }}
+                          >
+                            <DescriptionOutlinedIcon sx={{ fontSize: 16, mr: 0.5 }} />
+                            {lesson.lessonDesc
+                              ? lesson.lessonDesc.slice(0, 30) + "..."
+                              : lesson.lessonDesc}
+                          </Typography>
+                          <Typography
+                            level="body-xs"
+                            textColor="text.secondary"
+                            sx={{ fontWeight: 'md', display: 'flex', alignItems: 'center' }}
+                          >
+                            {lesson.lessonViews || 99}
+                            <VisibilityIcon sx={{ fontSize: 26, ml: 0.5 }} />
+                          </Typography>
+                        </CardContent>
                       </CardOverflow>
                     </Card>
                   </CssVarsProvider>
                 );
               })
             ) : (
-              <Box className="no-data">Popular products are not available!</Box>
+              <Box className="no-data">Popular lessons are not available!</Box>
             )}
           </Stack>
         </Stack>
